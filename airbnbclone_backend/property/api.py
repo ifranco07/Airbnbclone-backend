82e1815a89272067560455f401ff728227112c3e
ifranco07 <i.francomtz07@gmail.com>
2025-03-20 15:56:50 -0600

    10 Make it possible to set property as favorite

--- airbnbclone_backend/property/api.py
+++ airbnbclone_backend/property/api.py
@@ -1,16 +1,31 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
+from rest_framework_simplejwt.tokens import AccessToken
 from .forms import PropertyForm
 from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertyDetailSerializer, ReservationListSerializer
-
+from useraccount.models import User
 
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
 def properties_list(request):
+    #
+    #Auth
+    
+    try:
+        token = request.META['HTTP_AUTHORIZATION'].split('Bearer ')[1]
+        token = AccessToken(token)
+        user_id = token.payload['user_id']
+        user = User.objects.get(pk=user_id)
+    except Exception as e:
+        user = None
+        
+    #
+    #
+    
+    favorites = []
     properties = Property.objects.all()
     
     #
@@ -22,12 +37,21 @@ def properties_list(request):
         properties = properties.filter(landlord_id=landlord_id)
         
     #
+    #Favorites
+    
+    if user:
+        for property in properties:
+            if user in property.favorited.all():
+                favorites.append(property.id)
+                
+    #
     #
     
     serializer = PropertiesListSerializer(properties, many=True)
     
     return JsonResponse({
-        'data': serializer.data
+        'data': serializer.data,
+        'favorites': favorites
     })
     
     
@@ -93,4 +117,19 @@ def book_property(request, pk):
         return JsonResponse({'success': True})
     except Exception as e:
         print('Error', e)
+        
         return JsonResponse({'success': False, 'error': str(e)}, status=400)
+
+
+@api_view(['POST'])
+def toggle_favorite(request, pk):
+    property = Property.objects.get(pk=pk)
+    
+    if request.user in property.favorited.all():
+        property.favorited.remove(request.user)
+        
+        return JsonResponse({'is_favorite': False})
+    else:
+        property.favorited.add(request.user)
+        
+        return JsonResponse({'is_favorite': True})
