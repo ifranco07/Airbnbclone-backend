801f8df1c73625746dc0c9e8586e7e5788049908
ifranco07 <i.francomtz07@gmail.com>
2025-03-26 21:07:07 -0600

    15 Send/recive messages

--- airbnbclone_backend/chat/consumers.py
+++ airbnbclone_backend/chat/consumers.py
@@ -1,6 +1,6 @@
 import json
 
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .models import ConversationMessage
@@ -8,22 +8,52 @@ from .models import ConversationMessage
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = 'chat_{self.room_name}'
-        
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
         # Join room
-        
+
         await self.channel_layer.group_add(
-            self.room_group_name, 
+            self.room_group_name,
             self.channel_name
         )
-        
+
         await self.accept()
 
     async def disconnect(self):
-        #Leave room
-        
+        # Leave room
+
         await self.channel_layer.group_discard(
-            self.room_group_name, 
+            self.room_group_name,
             self.channel_name
-            )
+        )
+
+    # Recieve message from web sockets
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+
+        conversation_id = data['data']['conversation_id']
+        sent_to_id = data['data']['sent_to_id']
+        name = data['data']['name']
+        body = data['data']['body']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'body': body,
+                'name': name
+            }
+        )
+
+        await self.save_message(conversation_id, body, sent_to_id)
+    
+    # Sending messages
+    async def chat_message(self, event):
+        body = event['body']
+        name = event['name']
+
+        await self.send(text_data=json.dumps({
+            'body': body,
+            'name': name
+        }))
